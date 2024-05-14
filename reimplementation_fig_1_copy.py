91b125117695374c1c2f49bd7a6d94b1db274b27
Jakob Dichgans <jakob.dichgans@gmx.de>
2024-05-14 16:36:16 +0200

    get version without epsilon and dt: less steps per unit time, more for T, var, ar and ls on windows for T. Try to get get_lambda function right

--- reimplementation_fig_1_copy.py
+++ reimplementation_fig_1_copy.py
@@ -12,21 +12,20 @@ import string
 
 
 #define time interval, T should be integer
-T = 1000
+T = 5000
 
 def mu(t):
     return 2/T * t - 1
 
-epsilon = 1
-
 def f(x,t): 
-    return (x - x**3 /3.0 - mu(t))/epsilon
+    return (x - x**3 /3.0 - mu(t))
 
 
 
 # #get equilibria curves, n should be T+1
 
-def get_equilibria_paths(n):
+def get_equilibria_paths():
+    n = T + 1
     ts = np.linspace(0,T,n)
     upper = np.zeros(n)
     lower = np.zeros(n)
@@ -48,8 +47,8 @@ def get_equilibria_paths(n):
 
 #numerical simulation of SDEs:
 
-steps_per_unit_time = 1000                                              #must be high enough!
-ts = np.linspace(0,T,T*steps_per_unit_time + 1)
+steps_per_unit_time = 10                                             #must be high enough!
+solve_ts = np.linspace(0,T,T*steps_per_unit_time + 1)
 dt = 1/steps_per_unit_time
 
 
@@ -59,13 +58,13 @@ dt = 1/steps_per_unit_time
 xs_white = np.zeros(T*steps_per_unit_time + 1)
 xs_white[0] = 2.103803356099623
 
-sigma = 0.1
+sigma = 1
 white_noise = np.random.normal(0,np.sqrt(dt),T*steps_per_unit_time)
 
 x_new = xs_white[0]
 
 for i in tqdm.trange(T*steps_per_unit_time):
-    xs_white[i+1] = xs_white[i] + f(xs_white[i],ts[i])*dt + sigma*white_noise[i]
+    xs_white[i+1] = xs_white[i] + f(xs_white[i],solve_ts[i])*dt + sigma*white_noise[i]
 
 
 
@@ -73,7 +72,7 @@ for i in tqdm.trange(T*steps_per_unit_time):
 
 xs_red = np.zeros(T*steps_per_unit_time+1)
 xs_red[0] = 2.103803356099623                                                   #upper[0]
-kappa = 0.1                                                                      #shouldn't be too small 
+kappa = 1                                                                      #shouldn't be too small 
 
 def eta(theta):
     eta = np.zeros(T*steps_per_unit_time)
@@ -85,47 +84,50 @@ eta = eta(1)
 
 
 for i in tqdm.trange(T*steps_per_unit_time):
-    xs_red[i+1] = xs_red[i] + f(xs_red[i],ts[i])*dt + kappa*eta[i]*dt
+    xs_red[i+1] = xs_red[i] + f(xs_red[i],solve_ts[i])*dt + kappa*eta[i]*dt
 
 
 
 xs_white_filtered = xs_white[::steps_per_unit_time]
 xs_red_filtered = xs_red[::steps_per_unit_time]
 
+window_length = 50 
 
-def get_var(x,T):
-    #computes var of detrended time series for the T frames [0,1),...,[T-1,T)
-    #x should have size T*steps_per_unit_time + 1   
-    var = np.full(T+1,np.nan)
-    for i in tqdm.trange(T):
-        var[i+1] = statsmodels.tsa.tsatools.detrend(x[i*steps_per_unit_time:(i+1)*steps_per_unit_time],order=2).var()
+def get_var(x):
+    #get var of T-windows with length window length. Gives n_windows values
+    n_windows = int(T/window_length) 
+    var = np.full(n_windows,np.nan)
+    for i in tqdm.trange(n_windows):
+        var[i] = statsmodels.tsa.tsatools.detrend(x[i*window_length:(i+1)*window_length],order=2).var()
     return var
         
         
     
-def get_ar(x,T):
-    #x should have size T*steps_per_unit_time + 1
-    ar = np.full(T+1,np.nan)
-    for i in tqdm.tgrange(T):
-        ar[i+1] = statsmodels.tsa.stattools.acf(statsmodels.tsa.tsatools.detrend(x[i*steps_per_unit_time:(i+1)*steps_per_unit_time],order=2))[1]
+def get_ar(x):
+    #get ar of T-windows with length window length. Gives n_windows values
+    n_windows = int(T/window_length) 
+    ar = np.full(n_windows,np.nan)
+    for i in tqdm.tgrange(n_windows):
+        ar[i] = statsmodels.tsa.stattools.acf(statsmodels.tsa.tsatools.detrend(x[i*steps_per_unit_time:(i+1)*steps_per_unit_time],order=2))[1]
     return ar
 
 
 #ToDo: deal with size(eta) + 1 = size xs, should be fine?
 #vorher: (1/dt)**2, in Aufruf: dt = dt/epsilon, und wieso fs = 1/dt
 
-def get_lambdas(x,eta,dt,method="ratio"):
+def get_lambdas(x,noise,method="ratio"):
     
     def fitfunction(f,ls):
-        return (1/dt)/((2*np.pi*f)**2  + ls**2)          #omega = 2*pi*f ? why not 1 in nominator # vorher: (1/dt)**2
+        return 1/((2*np.pi*f)**2  + ls**2)          #omega = 2*pi*f ? why not 1 in nominator # vorher: (1/dt)**2
     
     
-    ls = np.full(T+1,np.nan)
+    n_windows = int(T/window_length) 
+    ls = np.full(n_windows,np.nan)
     
-    for i in tqdm.trange(T):
-        detx = statsmodels.tsa.tsatools.detrend(x[i*steps_per_unit_time:(i+1)*steps_per_unit_time],order=2)
-        f,Sxx = scipy.signal.welch(detx,fs=1/dt)                                    #was ist hier der frequency domain?
-        f,Sff = scipy.signal.welch(eta[i*steps_per_unit_time:(i+1)*steps_per_unit_time],fs=1/dt)  #,detrend="linear")
+    for i in tqdm.trange(n_windows):
+        detx = statsmodels.tsa.tsatools.detrend(x[i*window_length:(i+1)*window_length],order=2)
+        f,Sxx = scipy.signal.welch(detx)                                    #was ist hier der frequency domain?
+        f,Sff = scipy.signal.welch(noise[i*window_length:(i+1)*window_length])  #,detrend="linear")
             
         if method == "ratio":
             popt = scipy.optimize.curve_fit(fitfunction,
@@ -139,20 +141,20 @@ def get_lambdas(x,eta,dt,method="ratio"):
 
 
 
-variance_series_white = get_var(xs_white,T)
-variance_series_red = get_var(xs_red,T)
-ac_series_white = get_ar(xs_white,T)
-ac_series_red = get_ar(xs_red,T)
+variance_series_white = get_var(xs_white_filtered)
+variance_series_red = get_var(xs_red_filtered)
+ac_series_white = get_ar(xs_white_filtered)
+ac_series_red = get_ar(xs_red_filtered)
 
-ls_series_white = get_lambdas(xs_white,white_noise,dt)
-ls_series_red = get_lambdas(xs_red,eta,dt)
-
-
-log_ac_white = np.log(ac_series_white)/dt*epsilon                           #why epsilon?
-log_ac_red = np.log(ac_series_red)/dt*epsilon
+ls_series_white = get_lambdas(xs_white_filtered,white_noise[::window_length])
+ls_series_red = get_lambdas(xs_red_filtered,eta[::window_length])
 
 
-ts, upper, lower, unstable = get_equilibria_paths(T + 1)
+log_ac_white = np.log(ac_series_white)                           
+log_ac_red = np.log(ac_series_red)
+
+
+ts, upper, lower, unstable = get_equilibria_paths()
 
 tipp_upper = np.argmin(upper>1)
 tipp_lower = np.argmin(lower>-1)
@@ -171,26 +173,26 @@ axs[0].plot(ts,xs_white_filtered,color="blue")
 axs[0].plot(ts,xs_red_filtered,color="red")
 
 
-axs[1].plot(ts[:tip_white],log_ac_white[:tip_white],color="blue")                               #np.nan in first place or [1:tip_white]?
+axs[1].plot(ts[window_length:tip_white:window_length],log_ac_white[:int(tip_white/window_length)],color="blue")                               #np.nan in first place or [1:tip_white]?
 ax1_var = axs[1].twinx()
-ax1_var.plot(ts[1:tip_white],variance_series_white[1:tip_white],color="blue",linestyle="--")
+ax1_var.plot(ts[window_length:tip_white:window_length],variance_series_white[:int(tip_white/window_length)],color="blue",linestyle="--")
 
 
-axs[2].plot(ts[:tip_red],log_ac_red[:tip_red],color="red")
+axs[2].plot(ts[window_length:tip_red:window_length],log_ac_red[:int(tip_red/window_length)],color="red")
 ax2_var = axs[2].twinx()
-ax2_var.plot(ts[:tip_red],variance_series_red[:tip_red],color="red",linestyle="--")
+ax2_var.plot(ts[window_length:tip_red:window_length],variance_series_red[:int(tip_red/window_length)],color="red",linestyle="--")
 
 
 axs[0].set_ylabel(r"$x$")
-axs[1].set_ylabel(r"$\frac{1}{\Delta t} \log AC$")
-axs[2].set_ylabel(r"$\frac{1}{\Delta t} \log AC$")
+axs[1].set_ylabel(r"$\log AC$")
+axs[2].set_ylabel(r"$\log AC$")
 ax1_var.set_ylabel(r"Variance")
 ax2_var.set_ylabel(r"Variance")
 
 
 axs[3].set_xlabel("t")    
-axs[3].plot(ts[:tip_white],-ls_series_white[:tip_white],color="blue")
-axs[3].plot(ts[:tip_red],-ls_series_red[:tip_red],color="red")
+axs[3].plot(ts[window_length:tip_white:window_length],-ls_series_white[:int(tip_white/window_length)],color="blue")
+axs[3].plot(ts[window_length:tip_red:window_length],-ls_series_red[:int(tip_red/window_length)],color="red")
 # axs[3].plot(mus,-true_lambda,color="black")
 axs[3].set_ylabel(r"$\lambda$")
 
