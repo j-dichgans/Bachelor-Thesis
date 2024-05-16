daeae96919cdfdb9689a3258af5d60d832bf4b30
Jakob Dichgans <jakob.dichgans@gmx.de>
2024-05-16 09:58:22 +0200

    created clarke_fig_1_f_dynamics

--- reimplementation_fig_1_copy.py
+++ reimplementation_fig_1_copy.py
@@ -32,11 +32,11 @@ def get_equilibria_paths():
     unstable = np.zeros(n)
 
     for i in tqdm.trange(n):
-        sol = scipy.integrate.solve_ivp(lambda t,x: f(x,ts[i]),(0.0,100),[2.0],method="BDF")
+        sol = scipy.integrate.solve_ivp(lambda t,x: f(x,ts[i]),(0.0,50),[2.0])
         upper[i] = sol.y[0,-1]
-        sol = scipy.integrate.solve_ivp(lambda t,x: f(x,ts[i]),(0.0,100),[-2.0],method="BDF")
+        sol = scipy.integrate.solve_ivp(lambda t,x: f(x,ts[i]),(0.0,50),[-2.0])
         lower[i] = sol.y[0,-1]
-        sol = scipy.integrate.solve_ivp(lambda t,x: -f(x,ts[i]),(0.0,100),[0.0],method="BDF")
+        sol = scipy.integrate.solve_ivp(lambda t,x: -f(x,ts[i]),(0.0,50),[0.0])
         unstable[i] = sol.y[0,-1]
      
     unstable[np.abs(unstable)>1] = np.nan
@@ -44,10 +44,20 @@ def get_equilibria_paths():
     return ts,upper,lower,unstable
 
 
+def get_upper():
+    n = T + 1
+    ts = np.linspace(0,T,n)
+    upper = np.zeros(n)
+
+    for i in tqdm.trange(n):
+        sol = scipy.integrate.solve_ivp(lambda t,x: f(x,ts[i]),(0.0,50),[2.0])
+        upper[i] = sol.y[0,-1]
+
+    return ts, upper
 
 #numerical simulation of SDEs:
 
-steps_per_unit_time = 10                                             #must be high enough!
+steps_per_unit_time = 10                                             
 solve_ts = np.linspace(0,T,T*steps_per_unit_time + 1)
 dt = 1/steps_per_unit_time
 
@@ -58,10 +68,9 @@ dt = 1/steps_per_unit_time
 xs_white = np.zeros(T*steps_per_unit_time + 1)
 xs_white[0] = 2.103803356099623
 
-sigma = 1
+sigma = 0.1
 white_noise = np.random.normal(0,np.sqrt(dt),T*steps_per_unit_time)
 
-x_new = xs_white[0]
 
 for i in tqdm.trange(T*steps_per_unit_time):
     xs_white[i+1] = xs_white[i] + f(xs_white[i],solve_ts[i])*dt + sigma*white_noise[i]
@@ -71,8 +80,8 @@ for i in tqdm.trange(T*steps_per_unit_time):
 #red noise case
 
 xs_red = np.zeros(T*steps_per_unit_time+1)
-xs_red[0] = 2.103803356099623                                                   #upper[0]
-kappa = 1                                                                      #shouldn't be too small 
+xs_red[0] = 2.103803356099623                                                       #upper[0]
+kappa = 0.1                                                                        #shouldn't be too small 
 
 def eta(theta):
     eta = np.zeros(T*steps_per_unit_time)
@@ -141,24 +150,26 @@ def get_lambdas(x,noise,method="ratio"):
 
 
 
-variance_series_white = get_var(xs_white_filtered)
-variance_series_red = get_var(xs_red_filtered)
-ac_series_white = get_ar(xs_white_filtered)
-ac_series_red = get_ar(xs_red_filtered)
+# variance_series_white = get_var(xs_white_filtered)
+# variance_series_red = get_var(xs_red_filtered)
+# ac_series_white = get_ar(xs_white_filtered)
+# ac_series_red = get_ar(xs_red_filtered)
 
-ls_series_white = get_lambdas(xs_white_filtered,white_noise[::window_length])
-ls_series_red = get_lambdas(xs_red_filtered,eta[::window_length])
-
-
-log_ac_white = np.log(ac_series_white)                           
-log_ac_red = np.log(ac_series_red)
+# ls_series_white = get_lambdas(xs_white_filtered,white_noise[::window_length])
+# ls_series_red = get_lambdas(xs_red_filtered,eta[::window_length])
 
 
-ts, upper, lower, unstable = get_equilibria_paths()
+# log_ac_white = np.log(ac_series_white)                           
+# log_ac_red = np.log(ac_series_red)
 
-tipp_upper = np.argmin(upper>1)
-tipp_lower = np.argmin(lower>-1)
 
+# ts, upper, lower, unstable = get_equilibria_paths()
+
+# tipp_upper = np.argmin(upper>1)
+# tipp_lower = np.argmin(lower>-1)
+
+
+ts, upper = get_upper()
 
 tip_white = np.argmin(xs_white_filtered>1.0)
 tip_red = np.argmin(xs_red_filtered>1.0)  
@@ -166,35 +177,38 @@ tip_red = np.argmin(xs_red_filtered>1.0)
 
 fig,axs = plt.subplots(nrows=4,ncols=1,sharex=True,figsize=(8,10)) 
 
-axs[0].plot(ts[:tipp_upper],upper[:tipp_upper],color = "black")
-axs[0].plot(ts[tipp_lower:],lower[tipp_lower:],color = "black")
-axs[0].plot(ts[~np.isnan(unstable)],unstable[~np.isnan(unstable)],linestyle = "--")
-axs[0].plot(ts,xs_white_filtered,color="blue")
-axs[0].plot(ts,xs_red_filtered,color="red")
+# axs[0].plot(ts[:tipp_upper],upper[:tipp_upper],color = "black")
+# axs[0].plot(ts[tipp_lower:],lower[tipp_lower:],color = "black")
+# axs[0].plot(ts[~np.isnan(unstable)],unstable[~np.isnan(unstable)],linestyle = "--")
 
 
-axs[1].plot(ts[window_length:tip_white:window_length],log_ac_white[:int(tip_white/window_length)],color="blue")                               #np.nan in first place or [1:tip_white]?
-ax1_var = axs[1].twinx()
-ax1_var.plot(ts[window_length:tip_white:window_length],variance_series_white[:int(tip_white/window_length)],color="blue",linestyle="--")
+axs[0].plot(np.arange(301),upper[:301])
+axs[0].plot(np.arange(301),xs_white_filtered[:301],color="blue")
+axs[0].plot(np.arange(301),xs_red_filtered[:301],color="red")
 
 
-axs[2].plot(ts[window_length:tip_red:window_length],log_ac_red[:int(tip_red/window_length)],color="red")
-ax2_var = axs[2].twinx()
-ax2_var.plot(ts[window_length:tip_red:window_length],variance_series_red[:int(tip_red/window_length)],color="red",linestyle="--")
+# axs[1].plot(ts[window_length:tip_white:window_length],log_ac_white[:int(tip_white/window_length)],color="blue")                               
+# ax1_var = axs[1].twinx()
+# ax1_var.plot(ts[window_length:tip_white:window_length],variance_series_white[:int(tip_white/window_length)],color="blue",linestyle="--")
+
+
+# axs[2].plot(ts[window_length:tip_red:window_length],log_ac_red[:int(tip_red/window_length)],color="red")
+# ax2_var = axs[2].twinx()
+# ax2_var.plot(ts[window_length:tip_red:window_length],variance_series_red[:int(tip_red/window_length)],color="red",linestyle="--")
 
 
 axs[0].set_ylabel(r"$x$")
-axs[1].set_ylabel(r"$\log AC$")
-axs[2].set_ylabel(r"$\log AC$")
-ax1_var.set_ylabel(r"Variance")
-ax2_var.set_ylabel(r"Variance")
+# axs[1].set_ylabel(r"$\log AC$")
+# axs[2].set_ylabel(r"$\log AC$")
+# ax1_var.set_ylabel(r"Variance")
+# ax2_var.set_ylabel(r"Variance")
 
 
-axs[3].set_xlabel("t")    
-axs[3].plot(ts[window_length:tip_white:window_length],-ls_series_white[:int(tip_white/window_length)],color="blue")
-axs[3].plot(ts[window_length:tip_red:window_length],-ls_series_red[:int(tip_red/window_length)],color="red")
-# axs[3].plot(mus,-true_lambda,color="black")
-axs[3].set_ylabel(r"$\lambda$")
+# axs[3].set_xlabel("t")    
+# axs[3].plot(ts[window_length:tip_white:window_length],-ls_series_white[:int(tip_white/window_length)],color="blue")
+# axs[3].plot(ts[window_length:tip_red:window_length],-ls_series_red[:int(tip_red/window_length)],color="red")
+# # axs[3].plot(mus,-true_lambda,color="black")
+# axs[3].set_ylabel(r"$\lambda$")
 
 ax_0 = axs[0].twiny()
 ax_0.set_xlim(-1,1)
@@ -202,12 +216,6 @@ ax_0.set_xlabel("mu")
 
 plt.show()
 
-# axs[1].plot()
-
-
-# plt.plot(np.arange(T+1),xs_white_filtered, c='blue')
-# plt.plot(np.arange(T+1),xs_red_filtered, c='red')
-# plt.show()
 
 
 
